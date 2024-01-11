3e458dec74277e594d2a18324f98e8956a361709
Vassil Yanakiev <Vasil.Yanakiev@a1.bg>
2024-01-11 15:27:17 +0200

    update redis config

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { teamMembersRouter } from "./routes/team-members";
 import { teamsRouter } from "./routes/teams";
 import AppError from "./utils/appError";
 import { AppDataSource } from "./utils/data-source";
+import redisClient from "./utils/connectRedis";
 
 dotenv.config();
 const app = express();
@@ -14,9 +15,17 @@ AppDataSource.initialize()
     app.use(cors());
     app.use(express.json({ limit: "10kb" }));
 
+    app.get("/api/v1/healthChecker", async (_, res: Response) => {
+      const message = await redisClient.get("try");
+
+      res.status(200).json({
+        status: "success",
+        message,
+      });
+    });
+
     app.use("/api/v1", teamMembersRouter);
     app.use("/api/v1", teamsRouter);
-
     app.all("*", (req: Request, res: Response, next: NextFunction) => {
       next(new AppError(404, `Route ${req.originalUrl} not found`));
     });
